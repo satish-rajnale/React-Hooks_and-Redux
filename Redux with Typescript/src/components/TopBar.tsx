71785cfabfcae2af74b4e4885b2caee72e31137d
satish-rajnale <satishrajnale98@gmail.com>
2021-05-04 11:30:04 +0530

    json add

--- Redux with Typescript/src/components/TopBar.tsx
+++ Redux with Typescript/src/components/TopBar.tsx
@@ -1,16 +1,20 @@
 import * as React from "react";
 import { Button, Grid } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { useDispatch } from "react-redux"
+import { getTodos } from "../store/actions";
 
 /*
 JSON source: https://raw.githubusercontent.com/jherr/todos-four-ways/master/data/todos.json
 */
 
 function TopBar() {
+  const dispatch = useDispatch();
+
   return (
     <Grid pt={2} templateColumns="1fr 1fr" columnGap="3">
       <ColorModeSwitcher />
-      <Button>Load</Button>
+      <Button onClick={() => dispatch(getTodos("https://raw.githubusercontent.com/satish-rajnale/React-Hooks_and-Redux/main/Redux%20with%20Typescript/data/todos.json"))}>Load</Button>
     </Grid>
   );
 }
