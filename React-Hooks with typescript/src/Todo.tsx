a2cd20d12214fcd8decd1b747359b3c5017a04b2
satish-rajnale <satishrajnale98@gmail.com>
2021-07-30 09:06:48 +0530

    compo

--- React-Hooks with typescript/src/Todo.tsx
+++ React-Hooks with typescript/src/Todo.tsx
@@ -2,53 +2,6 @@
 import React, { useCallback, useEffect, useMemo, useReducer, useRef, useState } from "react";
 import Header from "./Header";
 import { getList } from "./placeholder";
-import {createGlobalState} from "react-use"
-
-type ACTIONTYPES = 
-| {type: "ADD"; text: String}
-| {type: "REMOVE"; id: number}
-
-interface Todo{
-  id: number;
-  text: String;
-  done: boolean;
-}
-const useGlobalTodos = createGlobalState<Todo[]>([]);
-
-export function useTodos(initialTodos: Todo[]):{
-  todos: Todo[];
-  addTodo: (text:string) => void;
-  removeTodo: (id: number) => void;
-}{
-
-  const [todos, setTodos] = useGlobalTodos();
-
-  useEffect(()=>{
-    setTodos(initialTodos)
-  },[initialTodos,setTodos]);
-
-  const addTodo = useCallback((text:string) => {
-    setTodos([...todos, {id:todos.length,text:text,done:false}])
-  },[todos,setTodos]);
-  const removeTodo = useCallback((removeid:number) => {
-    setTodos(todos.filter(({id}) => id != removeid));
-
-  },[todos,setTodos]);
-  // const [todos, dispatch] = useReducer((state: Todo[],action: ACTIONTYPES) => {
-  //   switch(action.type){
-  //     case "ADD":
-  //       return {
-  //         ...state,
-  //         {
-  //           id:number,
-  //           text:text,
-
-  //         }
-  //       }
-  //   }
-  // })
-  return {todos, addTodo,removeTodo};
-}
 
 
 const Todo = () =>{
