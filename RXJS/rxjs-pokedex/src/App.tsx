eb8dcc7121f991e0d26596be109ca21ed3a528f4
satish-rajnale <satishrajnale98@gmail.com>
2021-09-15 20:47:54 +0530

    implement simple filter with usememo

--- RXJS/rxjs-pokedex/src/App.tsx
+++ RXJS/rxjs-pokedex/src/App.tsx
@@ -1,20 +1,47 @@
   
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 // import { useObservableState } from "observable-hooks";
 import { BehaviorSubject, combineLatestWith, map } from "rxjs";
 
 import "./App.css";
 
-import { PokemonProvider, usePokemon } from "./store";
-function App() {
+import { Pokemon, rawPokemonwithPower$,  } from "./store";
 
+const Search = () => {
+  const [search, setsearch] = useState("");
+  const [Pokemon, setPokemon] = useState<Pokemon[]>([])
+  
+  
   useEffect(()=>{
     // rawPokemon$.subscribe(console.log);
-    rawPokemonwithPower$.subscribe(console.log);
+   const sub =  rawPokemonwithPower$.subscribe(setPokemon);
+    return () => sub.unsubscribe();
   },[]);
+
+  const filteredPokemon = useMemo(() => {
+    return Pokemon.filter((p) => p.name.toLowerCase().includes(search));
+  }, [Pokemon, search])
+
+  return(
+    <div>
+      <input
+      placeholder="search..."
+      type="search"
+      value={search}
+      onChange={(e) => setsearch(e.target.value)}
+      />
+      {filteredPokemon.map((p) => (
+        <div key={p.name}>
+          <strong>{p.name}</strong> - {p.power}
+        </div>
+      ))}
+    </div>
+  )
+}
+function App() {
   return (
-    <div className="App">
-    
+    <div style={{display:"grid", gridTemplateColumns:"1fr 1fr",}}>
+    <Search/>
     </div>
   );
 }
