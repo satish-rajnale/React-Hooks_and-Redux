e329fb895e6b9695dcd733cf707652c9743fb47e
satish-rajnale <satishrajnale98@gmail.com>
2021-12-12 20:11:59 +0530

    Update App.tsx

--- my-reactapp/src/App.tsx
+++ my-reactapp/src/App.tsx
@@ -55,85 +55,85 @@
 
 // export default App;
 import React from 'react';
-// // import './style.css';
+// import './style.css';
 
-// export default function App() {
-//   // React is loaded and is available as React and ReactDOM
-//   // imports should NOT be used
-//   return <List items={['A', 'B', 'C']} />;
-// }
+export default function App() {
+  // React is loaded and is available as React and ReactDOM
+  // imports should NOT be used
+  return <List items={['A', 'B', 'C']} />;
+}
 
-// const List = (props) => {
-//   const [listItems, setListItems] = React.useState([]);
+const List = (props) => {
+  const [listItems, setListItems] = React.useState([]);
 
-//   React.useEffect(() => {
-//     setListItems(props.items);
-//   }, []);
-//   React.useEffect(() => {
-//     console.log(listItems);
-//   }, [listItems]);
-//   const setOrder = (index) => {
-//     let currentList = listItems;
-//     let selectedItem = currentList.splice(index, 1);
-//     currentList.unshift(selectedItem[0]);
-//     // console.log(currentList);
-//     setListItems(currentList);
-//   };
-//   return (
-//     <ul>
-//       {listItems.map((item, index) => (
-//         <li key={index} onClick={() => setOrder(index)}>
-//           {item}
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// };
+  React.useEffect(() => {
+    setListItems(props.items);
+  }, []);
+  React.useEffect(() => {
+    console.log(listItems);
+  }, [listItems]);
+  const setOrder = (index) => {
+    let currentList = [...listItems];
+    let selectedItem = currentList.splice(index, 1);
+    currentList.unshift(selectedItem[0]);
+    console.log(currentList);
+    setListItems(currentList);
+  };
+  return (
+    <ul>
+      {listItems.map((item, index) => (
+        <li key={index} onClick={() => setOrder(index)}>
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 // React is loaded and is available as React and ReactDOM
 // imports should NOT be used
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      spanClicked: 0,
-      spanElements: [1, 2, 3, 4, 5],
-    };
-  }
-  setNo = (no) => {
-    this.setState({ spanClicked: no });
-  };
+// export default class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       spanClicked: 0,
+//       spanElements: [1, 2, 3, 4, 5],
+//     };
+//   }
+//   setNo = (no) => {
+//     this.setState({ spanClicked: no });
+//   };
 
-  render() {
-    return (
-      <div id="rating">
-        {/* @ts-ignore */}
-        {this.state.spanElements.map((item, index) => (
-          <SpanEL
-            key={item}
-            spanNo={item}
-            //@ts-ignore
-            spanClicked={this.state.spanClicked}
-            setNo={this.setNo}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div id="rating">
+//         {/* @ts-ignore */}
+//         {this.state.spanElements.map((item, index) => (
+//           <SpanEL
+//             key={item}
+//             spanNo={item}
+//             //@ts-ignore
+//             spanClicked={this.state.spanClicked}
+//             setNo={this.setNo}
+//           />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
 
-function SpanEL(props) {
-  let classes = props.spanNo <= props.spanClicked ? 'active' : '';
-  console.log(props.spanNo);
-  return (
-    <>
-      {classes !== '' ? (
-        <span className={classes} onClick={() => props.setNo(props.spanNo)}>
-          *
-        </span>
-      ) : (
-        <span onClick={() => props.setNo(props.spanNo)}>*</span>
-      )}
-    </>
-  );
-}
+// function SpanEL(props) {
+//   let classes = props.spanNo <= props.spanClicked ? 'active' : '';
+//   console.log(props.spanNo);
+//   return (
+//     <>
+//       {classes !== '' ? (
+//         <span className={classes} onClick={() => props.setNo(props.spanNo)}>
+//           *
+//         </span>
+//       ) : (
+//         <span onClick={() => props.setNo(props.spanNo)}>*</span>
+//       )}
+//     </>
+//   );
+// }
